20e70364319d0417b587efaa72f7832a1f335f8c
Max Kondratiuk <kondratiuk.11.maksim@gmail.com>
2022-09-24 21:00:12 +0300

    added new version

--- assets/js/maine.js
+++ assets/js/maine.js
@@ -1,25 +1,42 @@
-const translate = [
-    "translate(-200px)",
-    "translate(-100px)",
-    "translate(0px)",
-    "translate(100px)",
-    "translate(200px)",
+const translateX = [
+    "-400px",
+    "-300px",
+    "-200px",
+    "-100px",
+    "0px",
+    "100px",
+    "200px",
+    "300px",
+    "400px",
 ]
 
-const itemNum = () => {
-    return Math.floor(Math.random() * 4);
+const translateY = [
+    "-300px",
+    "-200px",
+    "-100px",
+    "0px",
+    "100px",
+    "200px",
+    "300px",
+]
+
+const itemNum = (positionArr) => {
+    return Math.floor(Math.random() * (positionArr.length - 1));
 }
 
 const duplicate = (prev, next) => {
     return prev === next
 }
 
-const changePosition = (itemNum, position) => {
-    itemNum.style.transform = translate[position];
+const translateParams = (positionX, positionY) => {
+    return `translate(${translateX[positionX]}, ${translateY[positionY]})`;
 }
 
-button_1.onmouseover = () => {
+const changePosition = (itemNum, positionX, positionY) => {
+    itemNum.style.transform = translateParams(positionX, positionY);
+}
 
+const crazyButton = () => {
     // 1) Перевіряємл чи є певний клас
     // 2) Якщо клас є то видаляємо його
     // 3)якщо класу немає то додаємо його
@@ -35,15 +52,26 @@ button_1.onmouseover = () => {
 
     // button_1.classList.toggle("active");
 
-    let position = itemNum();
-    let dup = duplicate(translate[position], button_1.style.transform);
+    let positionX = itemNum(translateX);
+    let positionY = itemNum(translateY);
+    let dup = duplicate(translateParams(positionX, positionY), button_1.style.transform);
 
     while (dup) {
-        position = itemNum();
-        dup = duplicate(translate[position], button_1.style.transform);
+        positionX = itemNum(translateX);
+        positionY = itemNum(translateY);
+        dup = duplicate(translateParams(positionX, positionY), button_1.style.transform);
     }
 
-    changePosition(button_1, position);
+    changePosition(button_1, positionX, positionY);
+}
+
+button_1.onmouseover = () => {
+    crazyButton();
+}
+
+button_1.onmousedown = () => {
+    console.log("тицьнули");
+    crazyButton();
 }
 
 // button_1.onmouseover = () => {
